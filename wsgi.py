7b96c28e8821d87cac1e8b6940f85022026258a1
Daniel <gruen.daniel@gmx.de>
2020-10-13 18:50:16 +0200

    Add select method to select season + episode

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,46 @@
-from flask import (Flask, render_template)
+import csv
+
+from flask import Flask, request
 from omxplayer.player import OMXPlayer
 
 # Create the web application instance
 app = Flask(__name__, template_folder="templates")
 
+# Season-episode-dictionary
+season_episode_csv_file = 'season_episode_dict.csv'
+reader = csv.reader(open(season_episode_csv_file, 'r'))
+season_episode_dict = {}
+for row in reader:
+    season, episode, link = row
+    season_episode_dict[(int(season), int(episode))] = link
 
-# Create a URL route in our application for "/"
+# Placeholder for player object
+player = None
+
+
+@app.route('/select')
+def select():
+    """
+    Select season and episode and start playing selected item.
+    E.g. http://192.168.0.134:5000/select?season=1&episode=2
+
+    :return: String with status
+    """
+    global player
+
+    if player:
+        player.stop()
+    season_ = request.args.get('season', default=1, type=int)
+    episode_ = request.args.get('episode', default='1', type=int)
+    video_link = season_episode_dict[(season_, episode_)]
+    #TODO: check if season episode combination valid
+
+    player = OMXPlayer(video_link, dbus_name='org.mpris.MediaPlayer2.omxplayer1')
+    player.play()
+
+    return "Playing season {}, episode {}.".format(season_, episode_)
+
+
 @app.route('/play')
 def play():
     """
@@ -13,9 +48,7 @@ def play():
 
     :return: String with status
     """
-    global player
-    video_path = 'https://www2080.o0-5.com/token=sYs-qvU4LC_3thExMsQgnA/1602613714/2a02:908::/27/1/29/753e64c2f00da07f81f72c5be86bf291-720p.mp4'
-    player = OMXPlayer(video_path, dbus_name='org.mpris.MediaPlayer2.omxplayer1')
+    player.play()
 
     return "Playing video."
 
@@ -34,4 +67,4 @@ def pause():
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
